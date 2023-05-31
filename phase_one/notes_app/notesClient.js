baa4ada2126bf2fa55f52dec083a51049f0d9767
George Barrett <georgeaustinbarrett@gmail.com>
2023-05-31 16:46:21 +0100

    loadNotes method in notesClient.js that fetches the notes data from the back end server (located in phase_three). associated test in notesClient.test.js. running GREEN

---
+++ phase_one/notes_app/notesClient.js
@@ -0,0 +1,13 @@
+class NotesClient {
+
+    loadNotes(callback) {
+        return fetch('http://localhost:3000/notes')
+        .then(response => response.json())
+        .then((data) => {
+            callback(data)
+        })
+    }
+
+}
+
+module.exports = NotesClient;
